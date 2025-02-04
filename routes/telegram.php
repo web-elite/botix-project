accbf8495c290c5b2623c0de3db41b2c981c5222
WebElite <webelitee@gmail.com>
2025-02-04 11:29:36 +0330

    nutgram handler

---
+++ routes/telegram.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Bot\StarterController;
+use App\Http\Middleware\GlobalBotMiddleware;
+use SergiX44\Nutgram\Nutgram;
+
+/*
+|--------------------------------------------------------------------------
+| Nutgram Handlers
+|--------------------------------------------------------------------------
+|
+| Here is where you can register telegram handlers for Nutgram. These
+| handlers are loaded by the NutgramServiceProvider. Enjoy!
+|
+*/
+
