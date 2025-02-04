8eb94ede57585b2baee911f813febd523e619b57
WebElite <webelitee@gmail.com>
2025-02-04 11:28:51 +0330

    add webhook url and controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
+Route::post('/webhook', [FrontController::class, '__invoke']);
 
 Route::get('/', function () {
     return view('welcome');
