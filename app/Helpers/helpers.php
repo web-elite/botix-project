58cd829f5684af1e7bc01a4afc45a5f93b88ce8b
WebElite <webelitee@gmail.com>
2025-03-30 00:48:14 +0330

    add helper file

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Str;
+
+if (! function_exists('escape_markdown')) {
+    function escape_markdown(string $text): string
+    {
+        $characters = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!'];
+        foreach ($characters as $char) {
+            $text = str_replace($char, '\\'.$char, $text);
+        }
+        return $text;
+    }
+}
+
+if (! function_exists('format_bytes')) {
+    function format_bytes(int $bytes, int $precision = 2): string
+    {
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+        
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+        
+        $bytes /= (1 << (10 * $pow));
+        
+        return round($bytes, $precision) . ' ' . $units[$pow];
+    }
+}
