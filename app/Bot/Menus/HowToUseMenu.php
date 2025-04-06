37afd6ae9cc57f03227d16cca1a344c64279d0eb
WebElite <webelitee@gmail.com>
2025-04-06 10:34:50 +0330

    add HowToUseMenu for user guidance

---
+++ app/Bot/Menus/HowToUseMenu.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Bot\Menus;
+
+use App\Models\User;
+use Pest\Plugins\Retry;
+use SergiX44\Nutgram\Conversations\InlineMenu;
+use SergiX44\Nutgram\Nutgram;
+use SergiX44\Nutgram\Telegram\Properties\ParseMode;
+use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardButton;
+
+class HowToUseMenu extends InlineMenu
+{
+
+    public function start(Nutgram $bot)
+    {
+        $this->clearButtons();
+
+        $this->menuText($message, ['parse_mode' => ParseMode::MARKDOWN])
+            ->addButtonRow(InlineKeyboardButton::make('آموزش اندروید', callback_data: "pause@handle"))
+            ->orNext('cancel')->showMenu();
+    }
+
+    public function cancel()
+    {
+        $bot->sendMessage(".\n🤔 چه کاری میخوای انجام بدی؟ از منو ربات انتخاب کن");
+    }
+}
