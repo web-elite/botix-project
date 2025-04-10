dd48d80fb5b7dbe8a231676e4e3c1bdf5db19e51
web-elite <webelitee@gmail.com>
2025-04-10 19:27:38 +0330

    refactor app.php to include API routing and prefix configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
+        api: __DIR__ . '/../routes/api.php',
+        apiPrefix: 'api',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
