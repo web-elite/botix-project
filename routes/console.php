107a645f396a49d665a758de45e3acfb8d762409
WebElite <webelitee@gmail.com>
2025-04-17 02:53:20 +0330

    feat: add command to notify users about expiring subscriptions daily

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,11 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
 
 Artisan::command('users:sync', function () {
     $this->comment('Syncing users...');
 })->purpose('Sync users with Telegram and Other Panels every 6 hours')->hourly();
+
+Artisan::command('notify:expiring-subscriptions', function () {
+    $this->comment('Notifying users about expiring subscriptions...');
+})->purpose('Notifying users daily')->daily();
